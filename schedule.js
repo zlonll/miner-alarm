1131dbd363dfc73eeffd2cf9bb249e3fc386a90d
zion <951093847@qq.com>
2022-02-18 19:16:59 +0800

    11

--- schedule.js
+++ schedule.js
@@ -3,8 +3,16 @@ var dateFormat = require('dateformat');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var rp = require('request-promise');
-var https = require('https')
-var sendEmail = require('./sendEmail')
+var sendEmail = require('./sendEmail');
+
+let rule1 = new schedule.RecurrenceRule();
+rule1.minute = [0, 10,11, 20, 30, 40, 50]; //
+rule1.second = 0;
+
+let rule2 = new schedule.RecurrenceRule();
+rule2.minute = [0,5,10,11,15,20,25,30,35,40,45,50,55]; //
+rule2.second = 0;
+
 const jobs = {
     alarm: async () => {
 
@@ -12,7 +20,7 @@ const jobs = {
             method: 'GET',
             uri: 'https://api.ethermine.org/miner/d3fbb8a8a84f52A8D3B010344A4dCf38F508B860/currentStats',
         };
-        schedule.scheduleJob('* /10/20/30/40/50/00 * * * *', async function () {
+        schedule.scheduleJob(rule1, async function () {
             rp(requestOptions).then((response) => {
                 console.log(response)
                 let res = JSON.parse(response)
@@ -35,7 +43,7 @@ const jobs = {
                 console.log(err)
             })
         })
-        schedule.scheduleJob('* /5/10/15/20/25/30/35/40/45/55/00 * * * *', async function () {
+        schedule.scheduleJob(rule2, async function () {
             rp(requestOptions).then((response) => {
                 console.log(response)
                 let res = JSON.parse(response)
