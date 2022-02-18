da2994ebf3fba40bbd5373b68fdc30bcff3b595d
zion <951093847@qq.com>
2022-02-18 16:15:10 +0800

    11

---
+++ schedule.js
@@ -0,0 +1,64 @@
+var schedule = require('node-schedule');
+var dateFormat = require('dateformat');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var rp = require('request-promise');
+var https = require('https')
+var sendEmail = require('./sendEmail')
+const jobs = {
+    alarm: async () => {
+
+        let requestOptions = {
+            method: 'GET',
+            uri: 'https://api.ethermine.org/miner/d3fbb8a8a84f52A8D3B010344A4dCf38F508B860/currentStats',
+        };
+        schedule.scheduleJob('* /10 * * * *', async function () {
+            rp(requestOptions).then((response) => {
+                console.log(response)
+                let res = JSON.parse(response)
+                console.log(res.data)
+                let data = res.data;
+                data.time = dateFormat(new Date(data.time * 1000), 'yyyy-mm-dd HH:MM:ss');
+                data.lastSeen = dateFormat(new Date(data.lastSeen * 1000), 'yyyy-mm-dd HH:MM:ss');
+                data.reportedHashrate = (data.reportedHashrate / 1000000).toFixed(2);
+                data.currentHashrate = (data.currentHashrate / 1000000).toFixed(2);
+                data.averageHashrate = (data.averageHashrate / 1000000).toFixed(2);
+                data.unpaid = (data.unpaid / 1000000000000000000).toFixed(7);
+                data.coinsPerMin = (data.coinsPerMin * 60 * 24);
+                data.usdPerMin = (data.coinsPerMin * 60 * 24);
+                data.btcPerMin = (data.staleShares / data.validShares * 100).toFixed(2)
+                console.log(data)
+                sendEmail.miner_message(res.status, data, (err) => {
+                    console.log(err)
+                })
+            }).catch((err) => {
+                console.log(err)
+            })
+        })
+        schedule.scheduleJob('* /5 * * * *', async function () {
+            rp(requestOptions).then((response) => {
+                console.log(response)
+                let res = JSON.parse(response)
+                let data = res.data;
+                data.time = dateFormat(new Date(data.time * 1000), 'yyyy-mm-dd HH:MM:ss');
+                data.lastSeen = dateFormat(new Date(data.lastSeen * 1000), 'yyyy-mm-dd HH:MM:ss');
+                if (res.status !== 'OK') {
+                    sendEmail.miner_warning(data, (err) => {
+                        console.log(err)
+                    })
+                }
+            }).catch((err) => {
+                console.log(err)
+            })
+        })
+    }
+};
+
+const scheduleJob = {
+    run: function () {
+        jobs.alarm();
+    },
+
+}
+
+module.exports = scheduleJob
---
+++ sendEmail.js
@@ -0,0 +1,96 @@
+var nodemailer = require("nodemailer");
+
+var smtp = "smtp.qq.com";
+
+function miner_message(status, data, callback) {
+    var transporter = nodemailer.createTransport({
+        host: smtp,
+        auth: {
+            user: "4683203@qq.com",
+            pass: 'nibdvrkgdeajbhge' //授权码,通过QQ获取
+
+        }
+    });
+
+    let stat = status === 'OK' ? `<span style="color:green;">ONLINE</span>` : `<span style="color:red;">OUTLINE</span>`
+
+
+    var mailOptions = {
+        from: '4683203@qq.com', // 发送者
+        to: 'zion18095610369@icloud.com', // 接受者,可以同时发送多个,以逗号隔开
+        subject: `miner message`, // 标题
+        html: `
+矿机状态：` + stat +
+            `<p>最后登陆时间：${data.lastSeen}</p>
+<p style="color: chocolate">上报算力：${data.reportedHashrate}</p>
+<p style="color: magenta">当前算力：${data.currentHashrate}</p>
+<p style="color: crimson">平均算力：${data.averageHashrate}</p>
+<p style="color: blue">陈旧占比：${data.btcPerMin}%</p>
+<p style="color: green">有效股份：${data.validShares}</p>
+<p style="color: red">无效股份：${data.invalidShares}</p>
+<p style="color: cornflowerblue">陈旧股份：${data.staleShares}</p>
+<p>活跃工人：${data.activeWorkers}</p>
+<p style="color: darkgreen">未付ETH：${data.unpaid}</p>
+<p style="color: darkmagenta">预估每日ETH收益：${data.coinsPerMin}</p>
+<p style="color: darkmagenta">预估每日USD收益：${data.usdPerMin}</p>
+`
+    };
+    var result = {
+        httpCode: 200,
+        message: '发送成功!',
+    }
+    try {
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                result.httpCode = 500;
+                result.message = err;
+                callback(result);
+                return;
+            }
+            callback(result);
+        });
+    } catch (err) {
+        result.httpCode = 500;
+        result.message = err;
+        callback(result);
+    }
+}
+
+function miner_warning(data, callback) {
+    var transporter = nodemailer.createTransport({
+        host: smtp,
+        auth: {
+            user: "951093847@qq.com",
+            pass: 'yisdtzespqbkbbfd' //授权码,通过QQ获取
+
+        }
+    });
+    var mailOptions = {
+        from: '951093847@qq.com', // 发送者
+        to: 'zion18095610369@icloud.com', // 接受者,可以同时发送多个,以逗号隔开
+        subject: `miner warning`, // 标题
+        html: `矿机状态：<span style="color:red;">OUTLINE</span><p>最后登陆时间：${data.lastSeen}</p>`
+    };
+    var result = {
+        httpCode: 200,
+        message: '发送成功!',
+    }
+    try {
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                result.httpCode = 500;
+                result.message = err;
+                callback(result);
+                return;
+            }
+            callback(result);
+        });
+    } catch (err) {
+        result.httpCode = 500;
+        result.message = err;
+        callback(result);
+    }
+}
+
+module.exports.miner_message = miner_message
+module.exports.miner_warning = miner_warning
