e90f74811ce4a5414a57f46668ee2ac1ca9dec7f
zion <951093847@qq.com>
2022-02-22 12:36:43 +0800

    v1.2

--- sendEmail.js
+++ sendEmail.js
@@ -7,7 +7,7 @@ function miner_message(status, data, callback) {
         host: smtp,
         auth: {
             user: "4683203@qq.com",
-            pass: 'nibdvrkgdeajbhge' //授权码,通过QQ获取
+            pass: 'dwsavqmbdbxmbieh' //授权码,通过QQ获取
 
         }
     });
